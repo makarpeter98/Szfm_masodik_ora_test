756c9764df0d90fd8143d19f1a3c16b73cf7ed7f
makar <makar.peter98@gmail.com>
2026-09-14 16:55:52 +0200

    Üres controller

--- src/main/java/org/example/szfm_harmadik_ora/HelloController.java
+++ src/main/java/org/example/szfm_harmadik_ora/HelloController.java
@@ -5,21 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-
-    Integer myNumber = 0;
-
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
     public void onPushMeButton(ActionEvent actionEvent) {
-
-        welcomeText.setText(myNumber.toString());
-        myNumber++;
-
+        System.out.println("Teszt!");
     }
 }
